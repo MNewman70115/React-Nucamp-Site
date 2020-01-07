03aa2c7c77c21de9fa88072a7def9a513fb0bfce
Matthew Newman <matt.newman@gmail.com>
2020-01-07 11:22:14 -0600

    Removed the h5 class from the cardimgoverlay class in AppCSS and from cardtitle in DirectoryComp

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -8,7 +8,7 @@ function RenderDirectoryItem({ campsite }) {
             <Link to={`/directory/${campsite.id}`}>
                 <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                 <CardImgOverlay>
-                    <CardTitle><h5>{campsite.name}</h5></CardTitle>
+                    <CardTitle>{campsite.name}</CardTitle>
                 </CardImgOverlay>
             </Link>
         </Card>
