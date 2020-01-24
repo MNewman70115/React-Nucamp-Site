fd454e4701ba59aeb0719a5a62ca0d982a0a9c05
Matthew Newman <matt.newman@gmail.com>
2020-01-24 12:52:51 -0600

    Campsite Comments Load Randomly Between 1 & 5 secs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { FadeTransform, Fade, Random } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -34,7 +34,7 @@ function RenderComments({ comments, postComment, campsiteId }) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                <Stagger in>
+                <Random minDelay={1000} maxDelay={5000} in>
                     {comments.map(comment => {
                         return (
                             <Fade in key={comment.id}>
@@ -46,7 +46,7 @@ function RenderComments({ comments, postComment, campsiteId }) {
                             </Fade>
                         );
                     })}
-                </Stagger>
+                </Random>
                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         )
